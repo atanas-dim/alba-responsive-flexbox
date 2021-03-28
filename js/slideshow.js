77e9d2c0ac6235790c9a73de676ec9c2b419a50a
atanasdim <hi.atanasdim@gmail.com>
2021-03-28 16:48:57 +0100

    slideshow.js update

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,8 +1,10 @@
-// 1. Grab each gallery
-// 2. Take total number of slides
-// 3. Define function that fades in/out each slide
-// 4. Next on click show next slide - if last slide then show first slide
-// 5. Prev on click show prev slide - if first slide then show last slide
+// 1. Save all galleries in a variable
+// 2. Declare functions that fades in/out each slide
+// 3. Declare function that checks the current slide number and rewind it at the beginning and at the end of the slides.
+// 4. Declare function that updates the curent and total slides number on screen
+// 5. Grab each gallery and call the previously declared functions according to the control link clicked.
+// 6. Next on click show next slide - if last slide then show first slide
+// 7. Prev on click show prev slide - if first slide then show last slide
 
 const galleries = document.querySelectorAll('.project-gallery');
 const galleriesArr = Array.from(galleries); // Array is needed in order to use forEach later.
@@ -15,36 +17,49 @@ const hideSlide = function (slide) {
     slide.style.opacity = 0;
 } 
 
+// If the control link has class Next, then increase the slide counter. Check if it has reached the last slide of the gallery, then rewind to the first slide
+// If the control link has class Prev, then decrease the slide counter. Check if it has reached the first slide of the gallery, then rewind to the last slide
+function checkCounter (counter, link, totalNum) {
+    if (link.classList.contains('next')) {
+        counter ++;
+        if (counter >= totalNum) {
+            counter = 0;
+        }
+    } else if (link.classList.contains('prev')) {
+        counter--;
+        if (counter < 0) {
+            counter = totalNum - 1;
+        }
+    }
+    return counter;
+}
+
+// Function that show the current and total slides below each gallery
+function updateSteps (htmlElem, current, total) {
+    htmlElem.innerHTML = `${current + 1} / ${total}`;
+}
+
 //  Grab each gallery. This is needed in order to avoid all of the galleries updating slides at the same time. That way each gallery works separately
 galleriesArr.forEach(gallery => {
 
     const allSlidesArr = Array.from(gallery.querySelectorAll('.slide-holder')); //Array is needed in order to call show/hide function for a specific slide with Array[]
     const totalSlidesNum = allSlidesArr.length;
-    let currentSlide = 0; //This is a counter which is used to change to prev/next slide and update the steps text in index.html
+    let currentSlide = 0; //This is a counter which is used to change to prev/next slide and update the steps text in index.html. It has to be individual for each gallery.
 
     const controlLinksArr  = Array.from(gallery.querySelectorAll('.control-link')); // Grabbing the two control links together to avoid repetitive code and only apply changes to the currentSlide counter
     const steps = gallery.querySelector('.steps');
 
-    steps.innerHTML = `${currentSlide + 1} / ${totalSlidesNum}`; // Steps have to be updated before click so that it shows corrent info on page load
+    updateSteps(steps, currentSlide, totalSlidesNum); // Steps have to be updated before click so that it shows correct info on page load
 
     // Apply event listener on Click to each control link
     controlLinksArr.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
 
-            // If the link has class Next, then increase currentSlide counter. Check if it has reached the last slide of the gallery, then rewind to the first slide
-            if (link.classList.contains('next')) {
-                currentSlide++;
-                if (currentSlide >= totalSlidesNum) {
-                    currentSlide = 0;
-                }   
-            // If the link has class Prev, then decrease the currentSLide counter. Check if it has reached the first slide of the gallery, then rewind to the last slide 
-            } else if (link.classList.contains('prev')) {
-                currentSlide--;   
-                if (currentSlide < 0) {
-                    currentSlide = totalSlidesNum - 1;
-                }
-            }
+            // Call the counter checker function to see if the currentSlide counter has reached 
+            // the end or the beginning of the slides. And rewind the currentSlide number accordingly. 
+            // Save this as the value of the currentSlide variable
+            currentSlide = checkCounter(currentSlide, link, totalSlidesNum);
 
             // Hide all slides on click
             allSlidesArr.forEach(slide => {
@@ -53,7 +68,7 @@ galleriesArr.forEach(gallery => {
 
             // Then show only the next slide and update steps text in index.html
             showSlide(allSlidesArr[currentSlide]);
-            steps.innerHTML = `${currentSlide + 1} / ${totalSlidesNum}`;
+            updateSteps(steps, currentSlide, totalSlidesNum);
 
         })
     })
